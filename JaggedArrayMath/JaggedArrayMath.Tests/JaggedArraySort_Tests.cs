05f9b07e260975564f443a87615b9a96eec9734e
Ilia Valchenko <ilia.valchenko.epam@gmail.com>
2016-07-07 23:50:42 +0300

    Add sorts for jagged arrays and unit tests.

---
+++ JaggedArrayMath/JaggedArrayMath.Tests/JaggedArraySort_Tests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static JaggedArrayMath.JaggedArraySort;
+
+namespace JaggedArrayMath.Tests
+{
+    [TestClass]
+    public class JaggedArraySort_Tests
+    {
+        [TestMethod]
+        public void SortBySomeOfRowElementsExpectedArraySortedByAscending()
+        {
+            int[][] jaggedArray = new int[][]
+            {
+                new int[] {1,3,5,7,9},
+                new int[] {0,2,4,6},
+                new int[] {11,22},
+                new int[] {1}
+            };
+            int[][] expectedArray = new int[][]
+            {
+                new int[] {1},
+                new int[] {0, 2, 4, 6},
+                new int[] {1, 3, 5, 7, 9},
+                new int[] {11, 22},               
+            };
+
+            SortBySumOfRowElements(jaggedArray);
+
+            Assert.IsTrue(IsEqualArrays(jaggedArray, expectedArray));
+        }
+
+        [TestMethod]
+        public void SortBySomeOfRowElementsExpectedArraySortedByDescending()
+        {
+            int[][] jaggedArray = new int[][]
+            {
+                new int[] {1,3,5,7,9},
+                new int[] {0,2,4,6},
+                new int[] {11,22},
+                new int[] {1}
+            };
+            int[][] expectedArray = new int[][]
+            {
+                new int[] {11, 22},
+                new int[] {1, 3, 5, 7, 9},
+                new int[] {0, 2, 4, 6},
+                new int[] {1}
+            };
+            bool isAscendingSort = false;
+
+            SortBySumOfRowElements(jaggedArray, isAscendingSort);
+
+            Assert.IsTrue(IsEqualArrays(jaggedArray, expectedArray));
+        }
+
+        [TestMethod]
+        public void SortBySomeOfRowElementsWithEmptyArraysExpectedArraySortedByAscending()
+        {
+            int[][] jaggedArray = new int[][]
+            {
+                new int[] {1,3,5,7,9},
+                new int[] {0,2,4,6},
+                new int[] {},
+                new int[] {11,22},
+                new int[] {1},
+                new int[] {}
+            };
+            int[][] expectedArray = new int[][]
+            {
+                new int[] {},
+                new int[] {},
+                new int[] {1},
+                new int[] {0, 2, 4, 6},
+                new int[] {1, 3, 5, 7, 9},
+                new int[] {11, 22},
+            };
+
+            SortBySumOfRowElements(jaggedArray);
+
+            Assert.IsTrue(IsEqualArrays(jaggedArray, expectedArray));
+        }
+
+        private bool IsEqualArrays(int[][] arr, int[][] brr)
+        {
+            if (arr.Length == brr.Length)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if(arr[i].Length != brr[i].Length)
+                        throw new ArgumentException("Arrays have different dimensions.");
+                }
+            }
+            else throw new ArgumentException("Arrays have different dimensions.");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    if (arr[i][j] != brr[i][j])
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ JaggedArrayMath/JaggedArrayMath/JaggedArraySort.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArrayMath
+{
+    /// <summary>
+    /// JaggedArraySort provides some of sorting methods for jagged arrays.
+    /// </summary>
+    public static class JaggedArraySort
+    {
+        #region sorting mathods for jagged arrays
+        /// <summary>
+        /// This method sorts jagged arrays by sum of elements in each row. 
+        /// </summary>
+        /// <param name="array">Array which have to be sorted</param>
+        /// <param name="isAscendingSort">Parameter which is responsible for ascending or descending sort</param>
+        public static void SortBySumOfRowElements(int[][] array, bool isAscendingSort = true)
+        {
+            for (int i = 0; i < array.Length; i++)
+                for (int j = array.Length - 1; j > i; j--)
+                {
+                    if (isAscendingSort)
+                    {
+                        if (GetSumOfRowElements(array[j]) < GetSumOfRowElements(array[j - 1]))
+                        {
+                            int[] helper = array[j];
+                            array[j] = array[j - 1];
+                            array[j - 1] = helper;
+                        }
+                    }
+                    else
+                    {
+                        if (GetSumOfRowElements(array[j]) > GetSumOfRowElements(array[j - 1]))
+                        {
+                            int[] helper = array[j];
+                            array[j] = array[j - 1];
+                            array[j - 1] = helper;
+                        }
+                    }
+                }
+        }
+
+        /// <summary>
+        /// This method sorts jagged arrays by max element in each row. 
+        /// </summary>
+        /// <param name="array">Array which have to be sorted</param>
+        /// <param name="isAscendingSort">Parameter which is responsible for ascending or descending sort</param>
+        public static void SortByRowMaxElement(int[][] array, bool isAscendingSort = true)
+        {
+            for (int i = 0; i < array.Length; i++)
+                for (int j = array.Length - 1; j > i; j--)
+                {
+                    if (isAscendingSort)
+                    {
+                        if (GetRowMaxElement(array[j]) < GetRowMaxElement(array[j - 1]))
+                        {
+                            int[] helper = array[j];
+                            array[j] = array[j - 1];
+                            array[j - 1] = helper;
+                        }
+                    }
+                    else
+                    {
+                        if (GetRowMaxElement(array[j]) > GetRowMaxElement(array[j - 1]))
+                        {
+                            int[] helper = array[j];
+                            array[j] = array[j - 1];
+                            array[j - 1] = helper;
+                        }
+                    }
+                }
+        }
+
+        /// <summary>
+        /// This method sorts jagged arrays by min element in each row. 
+        /// </summary>
+        /// <param name="array">Array which have to be sorted</param>
+        /// <param name="isAscendingSort">Parameter which is responsible for ascending or descending sort</param>
+        public static void SortByRowMinElement(int[][] array, bool isAscendingSort = true)
+        {
+            for (int i = 0; i < array.Length; i++)
+                for (int j = array.Length - 1; j > i; j--)
+                {
+                    if (isAscendingSort)
+                    {
+                        if (GetRowMinElement(array[j]) < GetRowMinElement(array[j - 1]))
+                        {
+                            int[] helper = array[j];
+                            array[j] = array[j - 1];
+                            array[j - 1] = helper;
+                        }
+                    }
+                    else
+                    {
+                        if (GetRowMinElement(array[j]) > GetRowMinElement(array[j - 1]))
+                        {
+                            int[] helper = array[j];
+                            array[j] = array[j - 1];
+                            array[j - 1] = helper;
+                        }
+                    }
+                }
+        } 
+        #endregion
+
+        #region private helper methods which are used in sorted methods
+        /// <summary>
+        /// This method finds sum of elements 
+        /// </summary>
+        /// <param name="row">Array of element</param>
+        /// <returns>Return sum of elements</returns>
+        private static int GetSumOfRowElements(int[] row)
+        {
+            int result = 0;
+
+            if (row.Length == 0)
+                result = Int32.MinValue;
+
+            for (int i = 0; i < row.Length; i++)
+                result += row[i];
+
+            return result;
+        }
+
+        /// <summary>
+        /// This method finds max of elements 
+        /// </summary>
+        /// <param name="row">Array of element</param>
+        /// <returns>Return max of elements</returns>
+        private static int GetRowMaxElement(int[] row)
+        {
+            int result = 0;
+
+            if (row.Length == 0)
+                result = Int32.MinValue;
+
+            if (row.Length >= 1)
+                result = row[0];
+
+            for (int i = 1; i < row.Length; i++)
+                if (row[i] > result)
+                    result = row[i];
+
+            return result;
+        }
+
+        /// <summary>
+        /// This method finds min of elements 
+        /// </summary>
+        /// <param name="row">Array of element</param>
+        /// <returns>Return min of elements</returns>
+        private static int GetRowMinElement(int[] row)
+        {
+            int result = 0;
+
+            if (row.Length == 0)
+                result = Int32.MinValue;
+
+            if (row.Length >= 1)
+                result = row[0];
+
+            for (int i = 1; i < row.Length; i++)
+                if (row[i] < result)
+                    result = row[i];
+
+            return result;
+        } 
+        #endregion
+    }
+}

08349a4a91091a939aeed4253ce6b65c7701f3d1
Ilia Valchenko <ilia.valchenko.epam@gmail.com>
2016-07-10 16:27:13 +0300

    Method which converts Double to IEEE 754 using BitConventer class was added.

---
+++ DoubleToIEEE754/DoubleToIEEE754.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DoubleToIEEE754.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [TestCase(17.12, TestName = "17.12ToIEEE754UsingBitConventer", ExpectedResult = "0100000000110001000111101011100001010001111010111000010100011111")]
+        [TestCase(15458799817.15458, TestName = "15458799817.15458ToIEEE754UsingBitConventer", ExpectedResult = "0100001000001100110010110101010010000110010010010011110010010100")]
+        [TestCase(1.00002, TestName = "1.00002ToIEEE754UsingBitConventer", ExpectedResult = "0011111111110000000000000001010011111000101101011000100011100011")]
+        public string TestDoubleToIEEE754UsingBitConventer(double number)
+        {
+            return number.ToIEEE754UsingBitConverter();
+        }
+    }
+}
---
+++ DoubleToIEEE754/DoubleToIEEE754/DoubleExtension.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleToIEEE754
+{
+    /// <summary>
+    /// This is an extension class for System.Double which provides converting methods to IEEE 754 standard.
+    /// </summary>
+    public static class DoubleExtension
+    {
+        /// <summary>
+        /// This method convert double into string which represents it in IEEE 754 using BitConverter. 
+        /// </summary>
+        /// <param name="db">Double number which must be converted</param>
+        /// <returns>String representation of double in IEEE 754</returns>
+        public static string ToIEEE754UsingBitConverter(this Double db)
+        {
+            var bitArray = new BitArray(BitConverter.GetBytes(db));
+            StringBuilder res = new StringBuilder(64);
+
+            for (int i = bitArray.Length - 1; i >= 0; i--)
+            {
+                if (bitArray[i] == false)
+                    res.Append('0');
+                else
+                    res.Append('1');
+            }
+
+            return res.ToString();
+        }
+
+        /// <summary>
+        /// This method convert double into string which represents it in IEEE 754. 
+        /// </summary>
+        /// <param name="db">Double number which must be converted</param>
+        /// <returns>String representation of double in IEEE 754</returns>
+        public static string ToIEEE754(this double db)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
